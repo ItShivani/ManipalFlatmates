b2c490c4c59b8ce8c37b90167395357a2c8ca6b7
ItShivani <shivanisai26@gmail.com>
2021-05-21 15:40:13 +0530

    register and login

---
+++ index.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const {ApolloServer} = require('apollo-server');
+
+const typeDefs = require('./graphql/typeDefs');
+const { MONGODB } = require('./config.js');
+const resolvers = require('./graphql/resolvers');
+
+
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers
+});
+
+mongoose.connect(MONGODB,{useNewUrlParser:true})
+	.then(() => {
+		console.log("Connected to MongoDB");
+		return server.listen({port:5000});
+	})
+	.then(res => {
+		console.log(`Server running at ${res.url}`)
+	});
