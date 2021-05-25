db12f5a46649e279bff19c8e86310cf54482c00a
ItShivani <shivanisai26@gmail.com>
2021-05-25 21:04:24 +0530

    added semantic ui elements for login and register

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import MenuTab from './components/MenuTab';
 
 function App() {
   return (
-    <div>
-    <h1> Welcome to Manipal Flatmates </h1>
-    </div>
+    <Router> 
+      <Container>
+        <MenuTab/>
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/register' component={Register}/>
+      </Container>
+    </Router>
   );
 }
 
